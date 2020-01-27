d15b1601e0a6cff00fb562a93f6d0bf3f31459a5
Nathan <48669774+Zexxerd@users.noreply.github.com>
2020-01-26 23:37:52 -0500

    Updated alpha.c

--- src/alpha.c
+++ src/alpha.c
@@ -13,17 +13,17 @@
 #include <debug.h>
 #include "alpha.h"
 
-//0b0111110000000000,0b0000001111100000,0b0000000000011111
+//0b0111110000000000
+//0b0000001111100000
+//0b0000000000011111
 #define red_part(c) ((c & 31744) >> 10)
 #define green_part(c) (((c & 992) >> 4) | ((c & 1<<16) >> 16)) //puts the lsbit at the end for easy maths.
 #define blue_part(c) (c & 31)
 
-//uint16_t alpha_color;
 void set_alpha_color(uint16_t color) {
     //Assuming a palette of 128 colors with one transparent color, in rgb1555 format
     //rgb 1555 is 1 bit green (lsbit), 5 bits red, 5 bits green (msbits), and 5 bits blue.
     uint8_t r,g,b;
-    uint16_t temp_color;
     uint8_t i;
     for (i = 0;i<128;i++) {
         r =   red_part(gfx_palette[i]);
@@ -36,7 +36,7 @@ void set_alpha_color(uint16_t color) {
     }
 }
 void alpha_mask_sprite(gfx_sprite_t * sprite,int x,int y) {
-    //not having a transparent index is the same as inputting a square as input.
+    //not using a transparent index would be the same as inputting a rectangle.
     uint16_t i;
     uint8_t transparent_index = gfx_SetTransparentColor(0);
     gfx_SetTransparentColor(transparent_index);
